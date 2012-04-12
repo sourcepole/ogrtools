1db5249d4eef471133daf62d6dc4107a4ebb6a55
Pirmin Kalberer <pka@sourcepole.ch>
2012-04-12 11:40:00 +0200

    Sextante example algorithm

--- ogrprocessing/ogrprocessingplugin.py
+++ ogrprocessing/ogrprocessingplugin.py
@@ -28,6 +28,15 @@ import resources_rc
 # Import the code for the dialog
 from ogrprocessingplugindialog import OgrProcessingPluginDialog
 
+import os, sys
+import inspect
+from sextante.core.Sextante import Sextante
+from sextanteexampleprovider.ExampleAlgorithmProvider import ExampleAlgorithmProvider
+
+cmd_folder = os.path.split(inspect.getfile( inspect.currentframe() ))[0]
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+
 class OgrProcessingPlugin:
 
     def __init__(self, iface):
@@ -50,6 +59,8 @@ class OgrProcessingPlugin:
 
             if qVersion() > '4.3.3':
                 QCoreApplication.installTranslator(self.translator)
+
+        self.provider = ExampleAlgorithmProvider()
    
 
     def initGui(self):
@@ -63,10 +74,13 @@ class OgrProcessingPlugin:
         self.iface.addToolBarIcon(self.action)
         self.iface.addPluginToMenu(u"&OGR Processing", self.action)
 
+        Sextante.addProvider(self.provider)
+
     def unload(self):
         # Remove the plugin menu item and icon
         self.iface.removePluginMenu(u"&OGR Processing",self.action)
         self.iface.removeToolBarIcon(self.action)
+        Sextante.removeProvider(self.provider)
 
     # run method that performs all the real work
     def run(self):
