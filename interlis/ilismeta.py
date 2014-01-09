f3151570e9aee1716dc8315691d0c03368e0e559
Pirmin Kalberer <pka@sourcepole.ch>
2014-01-09 23:39:16 +0100

    Generate dot graph from IlisMeta

--- interlis/ilismeta.py
+++ interlis/ilismeta.py
@@ -125,11 +125,63 @@ def extract_enums_json(fn):
     return enum_tables
 
 
+def nodeid(tid):
+    return tid.replace(".", "_")
+
+
+def imd_to_dot(fn):
+    """Generate dot graph from IlisMeta file"""
+    tree = ElementTree.parse(fn)
+    #Extract default namespace from root e.g. {http://www.interlis.ch/INTERLIS2.3}TRANSFER
+    ns = {'xmlns': re.match(r'^{(.+)}', tree.getroot().tag).group(1)}
+
+    print "digraph {"
+    models = tree.findall("xmlns:DATASECTION/xmlns:IlisMeta07.ModelData", ns) or []
+    modelno = 0
+    for model in models:
+        bid = nodeid(model.get("BID"))
+        if bid == 'MODEL_INTERLIS':
+            continue
+        print "subgraph {"
+        modelno = modelno + 1
+        print "node [style=filled,colorscheme=accent8,color={}]".format(modelno)
+        for node in model:
+            tag = node.tag.replace("{http://www.interlis.ch/INTERLIS2.3}IlisMeta07.ModelData.", "")
+            if node.get("TID"):
+                tid = nodeid(node.get("TID"))
+                name = node.find("xmlns:Name", ns).text
+                print tid + ' [label="' + name + "\\n" + tag + '"]'
+                for refnode in node.findall("./*[@REF]"):
+                    reftag = refnode.tag.replace("{http://www.interlis.ch/INTERLIS2.3}", "")
+                    orderpos = refnode.get("ORDER_POS")
+                    if orderpos:
+                        reftag = reftag + "[{}]".format(orderpos)
+                    if reftag != 'ElementInPackage':
+                        print tid + " -> " + nodeid(refnode.get("REF")) + ' [label="' + reftag + '"]'
+            else:
+                relnodes = node.findall("./*[@REF]")
+                n1 = nodeid(relnodes[0].get("REF"))
+                #l1 = relnodes[0].tag.replace("{http://www.interlis.ch/INTERLIS2.3}", "")
+                n2 = nodeid(relnodes[1].get("REF"))
+                #l2 = relnodes[1].tag.replace("{http://www.interlis.ch/INTERLIS2.3}", "")
+                #print n1 + " -> " + n2 + ' [headlabel="' + l2 + '" taillabel="' + l1 + '" style=dotted]'
+                print n1 + " -> " + n2 + ' [label="' + tag + '" style=dotted]'
+
+        print "}"
+    print "}"
+
+
 def main(argv):
-    fn = argv[1]
-    #print prettify(extract_enums_asgml(fn))
-    enum_tables = extract_enums_json(fn)
-    print json.dumps(enum_tables, indent=2)
+    output = argv[1]
+    fn = argv[2]
+    if output == 'enumgml':
+        print prettify(extract_enums_asgml(fn))
+    elif output == 'enumjson':
+        enum_tables = extract_enums_json(fn)
+        print json.dumps(enum_tables, indent=2)
+    elif output == 'dot':
+        #./ilismeta.py dot ../tests/data/ili/RoadsExdm2ien.imd | dot -Tps >/tmp/imd.ps
+        imd_to_dot(fn)
     return 0
 
 if __name__ == '__main__':
