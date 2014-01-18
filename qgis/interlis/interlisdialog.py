c860dedcb2bd86709ebf27c668eb07f6b3c43b14
Pirmin Kalberer <pka@sourcepole.ch>
2014-01-18 23:56:11 +0100

    Import dialog

--- qgis/interlis/interlisdialog.py
+++ qgis/interlis/interlisdialog.py
@@ -20,9 +20,10 @@
  ***************************************************************************/
 """
 
-from PyQt4 import QtCore, QtGui
+from PyQt4 import QtGui
+from PyQt4.QtCore import pyqtSignature, QSettings, QFileInfo
+from PyQt4.QtGui import QFileDialog, QDialogButtonBox
 from ui_interlis import Ui_Interlis
-# create the dialog for zoom to point
 
 
 class InterlisDialog(QtGui.QDialog):
@@ -31,3 +32,34 @@ class InterlisDialog(QtGui.QDialog):
         # Set up the user interface from Designer.
         self.ui = Ui_Interlis()
         self.ui.setupUi(self)
+
+    def dataSourceUri(self):
+        if self.ui.mDataLineEdit.text().isEmpty():
+            return ""
+        if not self.ui.mModelLineEdit.text().isEmpty():
+            return self.ui.mDataLineEdit.text() + "," + self.ui.mModelLineEdit.text()
+        else:
+            return self.ui.mDataLineEdit.text()
+
+    @pyqtSignature('')  # avoid two connections
+    def on_mDataFileButton_clicked(self):
+        #show file dialog and remember last directory
+        settings = QSettings()
+        lastDirectoryString = None  # settings.value("/qgis/plugins/interlis/datadir", type=str)
+        dataFilePath = QFileDialog.getOpenFileName(None, "Open Interlis data file", lastDirectoryString, "*.itf *.ITF *.xtf *.XTF *.xml")
+        if dataFilePath is None:
+            return  # dialog canceled
+        settings.setValue("/qgis/plugins/interlis/datadir", QFileInfo(dataFilePath).absolutePath())
+        self.ui.mDataLineEdit.insert(dataFilePath)
+        self.ui.buttonBox.button(QDialogButtonBox.Ok).setEnabled(True)
+
+    @pyqtSignature('')  # avoid two connections
+    def on_mModelFileButton_clicked(self):
+        #show file dialog and remember last directory
+        settings = QSettings()
+        lastDirectoryString = None  # settings.value("/qgis/plugins/interlis2/modeldir", type=str)
+        modelFilePath = QFileDialog.getOpenFileName(None, "Open Interlis data file", lastDirectoryString, "*.imd *.IMD")
+        if modelFilePath is None:
+            return  # dialog canceled
+        settings.setValue("/qgis/plugins/interlis/modeldir", QFileInfo(modelFilePath).absolutePath())
+        self.ui.mModelLineEdit.insert(modelFilePath)
