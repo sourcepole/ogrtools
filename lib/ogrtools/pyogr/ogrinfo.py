492739e96808a082ec0d531f6940fac7b27a8fab
Pirmin Kalberer <pka@sourcepole.ch>
2014-01-23 21:29:29 +0100

    update to ogrinfo from OGR/GDAL 1.11dev

--- lib/ogrtools/pyogr/ogrinfo.py
+++ lib/ogrtools/pyogr/ogrinfo.py
@@ -1,5 +1,38 @@
 #!/usr/bin/env python
-# Based on ogrinfo.py implementation by Even Rouault included in GDAL/OGR
+#/******************************************************************************
+# * $Id$
+# *
+# * Project:  OpenGIS Simple Features Reference Implementation
+# * Purpose:  Python port of a simple client for viewing OGR driver data.
+# * Author:   Even Rouault, <even dot rouault at mines dash paris dot org>
+# *
+# * Port from ogrinfo.cpp whose author is Frank Warmerdam
+# *
+# ******************************************************************************
+# * Copyright (c) 2010, Even Rouault
+# * Copyright (c) 1999, Frank Warmerdam
+# *
+# * Permission is hereby granted, free of charge, to any person obtaining a
+# * copy of this software and associated documentation files (the "Software"),
+# * to deal in the Software without restriction, including without limitation
+# * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# * and/or sell copies of the Software, and to permit persons to whom the
+# * Software is furnished to do so, subject to the following conditions:
+# *
+# * The above copyright notice and this permission notice shall be included
+# * in all copies or substantial portions of the Software.
+# *
+# * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# * DEALINGS IN THE SOFTWARE.
+# ****************************************************************************/
+
+# Note : this is the most direct port of ogrinfo.cpp possible
+# It could be made much more Python'ish !
 
 import sys
 
@@ -10,9 +43,11 @@ except:
     import gdal
     import ogr
 
-bVerbose_ = True
-bSummaryOnly_ = False
-nFetchFID_ = ogr.NullFID
+bReadOnly = False
+bVerbose = True
+bSummaryOnly = False
+nFetchFID = ogr.NullFID
+papszOptions = None
 
 def EQUAL(a, b):
     return a.lower() == b.lower()
@@ -23,9 +58,11 @@ def EQUAL(a, b):
 
 def main(argv = None):
     
-    bVerbose = True
-    bSummaryOnly = False
-    nFetchFID = ogr.NullFID
+    global bReadOnly
+    global bVerbose
+    global bSummaryOnly
+    global nFetchFID
+    global papszOptions
     
     pszWHERE = None
     pszDataSource = None
@@ -36,6 +73,7 @@ def main(argv = None):
     pszSQLStatement = None
     pszDialect = None
     options = {}
+    pszGeomField = None
 
     if argv is None:
         argv = sys.argv
@@ -77,6 +115,10 @@ def main(argv = None):
             poSpatialFilter = ogr.Geometry(ogr.wkbPolygon)
             poSpatialFilter.AddGeometry(oRing)
             iArg = iArg + 4
+
+        elif EQUAL(argv[iArg],"-geomfield") and iArg < nArgc-1:
+            iArg = iArg + 1
+            pszGeomField = argv[iArg]
 
         elif EQUAL(argv[iArg],"-where") and iArg < nArgc-1:
             iArg = iArg + 1
@@ -122,59 +164,6 @@ def main(argv = None):
     if pszDataSource is None:
         return Usage()
 
-    return ogrinfo(bReadOnly,
-        bVerbose,
-        bSummaryOnly,
-        nFetchFID,
-        pszWHERE,
-        pszDataSource,
-        papszLayers,
-        poSpatialFilter,
-        nRepeatCount,
-        bAllLayers,
-        pszSQLStatement,
-        pszDialect,
-        options)
-
-
-def ogr_formats():
-    for iDriver in range(ogr.GetDriverCount()):
-        print( "  -> %s" % ogr.GetDriver(iDriver).GetName() )
-
-def ogr_version_info():
-    try:
-        return gdal.VersionInfo('RELEASE_NAME')
-    except:
-        return "Unknown"
-
-def ogr_version_num():
-    try:
-        return int(gdal.VersionInfo('VERSION_NUM'))
-    except:
-        return 1000
-
-def ogrinfo(bReadOnly = False,
-            bVerbose = True,
-            bSummaryOnly = False,
-            nFetchFID = ogr.NullFID,
-            pszWHERE = None,
-            pszDataSource = None,
-            papszLayers = None,
-            poSpatialFilter = None,
-            nRepeatCount = 1,
-            bAllLayers = False,
-            pszSQLStatement = None,
-            pszDialect = None,
-            options = {}):
-
-    global bVerbose_
-    global bSummaryOnly_
-    global nFetchFID_
-
-    bVerbose_ = bVerbose
-    bSummaryOnly_ = bSummaryOnly
-    nFetchFID_ = nFetchFID
-
 #/* -------------------------------------------------------------------- */
 #/*      Open data source.                                               */
 #/* -------------------------------------------------------------------- */
@@ -195,7 +184,8 @@ def ogrinfo(bReadOnly = False,
         print( "FAILURE:\n"
                 "Unable to open datasource `%s' with the following drivers." % pszDataSource )
 
-        ogr_formats()
+        for iDriver in range(ogr.GetDriverCount()):
+            print( "  -> %s" % ogr.GetDriver(iDriver).GetName() )
 
         return 1
 
@@ -226,8 +216,11 @@ def ogrinfo(bReadOnly = False,
         if papszLayers is not None:
             print( "layer names ignored in combination with -sql." )
 
-        poResultSet = poDS.ExecuteSQL( pszSQLStatement, poSpatialFilter, 
-                                        pszDialect )
+        if pszGeomField is None:
+            poResultSet = poDS.ExecuteSQL( pszSQLStatement, poSpatialFilter, 
+                                            pszDialect )
+        else:
+            poResultSet = poDS.ExecuteSQL( pszSQLStatement, None, pszDialect )
 
         if poResultSet is not None:
             if pszWHERE is not None:
@@ -235,7 +228,10 @@ def ogrinfo(bReadOnly = False,
                     print("FAILURE: SetAttributeFilter(%s) failed." % pszWHERE)
                     return 1
 
-            ReportOnLayer( poResultSet, None, None, options )
+            if pszGeomField is not None:
+                ReportOnLayer( poResultSet, None, pszGeomField, poSpatialFilter, options )
+            else:
+                ReportOnLayer( poResultSet, None, None, None, options )
             poDS.ReleaseResultSet( poResultSet )
 
     #gdal.Debug( "OGR", "GetLayerCount() = %d\n", poDS.GetLayerCount() )
@@ -255,6 +251,16 @@ def ogrinfo(bReadOnly = False,
                 if not bAllLayers:
                     line = "%d: %s" % (iLayer+1, poLayer.GetLayerDefn().GetName())
 
+                    nGeomFieldCount = poLayer.GetLayerDefn().GetGeomFieldCount()
+                    if nGeomFieldCount > 1:
+                        line = line + " ("
+                        for iGeom in range(nGeomFieldCount):
+                            if iGeom > 0:
+                                line = line + ", "
+                            poGFldDefn = poLayer.GetLayerDefn().GetGeomFieldDefn(iGeom)
+                            line = line + "%s" % ogr.GeometryTypeToName( poGFldDefn.GetType() )
+                        line = line + ")"
+
                     if poLayer.GetLayerDefn().GetGeomType() != ogr.wkbUnknown:
                         line = line + " (%s)" % ogr.GeometryTypeToName( poLayer.GetLayerDefn().GetGeomType() )
 
@@ -263,7 +269,7 @@ def ogrinfo(bReadOnly = False,
                     if iRepeat != 0:
                         poLayer.ResetReading()
 
-                    ReportOnLayer( poLayer, pszWHERE, poSpatialFilter, options )
+                    ReportOnLayer( poLayer, pszWHERE, pszGeomField, poSpatialFilter, options )
 
         else:
 #/* -------------------------------------------------------------------- */ 
@@ -279,7 +285,7 @@ def ogrinfo(bReadOnly = False,
                 if iRepeat != 0:
                     poLayer.ResetReading()
 
-                ReportOnLayer( poLayer, pszWHERE, poSpatialFilter, options )
+                ReportOnLayer( poLayer, pszWHERE, pszGeomField, poSpatialFilter, options )
 
 #/* -------------------------------------------------------------------- */
 #/*      Close down.                                                     */
@@ -295,7 +301,7 @@ def ogrinfo(bReadOnly = False,
 def Usage():
 
     print( "Usage: ogrinfo [--help-general] [-ro] [-q] [-where restricted_where]\n"
-            "               [-spat xmin ymin xmax ymax] [-fid fid]\n"
+            "               [-spat xmin ymin xmax ymax] [-geomfield field] [-fid fid]\n"
             "               [-sql statement] [-al] [-so] [-fields={YES/NO}]\n"
             "               [-geom={YES/NO/SUMMARY}][--formats]\n"
             "               datasource_name [layer [layer ...]]")
@@ -305,7 +311,7 @@ def Usage():
 #/*                           ReportOnLayer()                            */
 #/************************************************************************/
 
-def ReportOnLayer( poLayer, pszWHERE, poSpatialFilter, options ):
+def ReportOnLayer( poLayer, pszWHERE, pszGeomField, poSpatialFilter, options ):
 
     poDefn = poLayer.GetLayerDefn()
 
@@ -318,7 +324,14 @@ def ReportOnLayer( poLayer, pszWHERE, poSpatialFilter, options ):
             return
 
     if poSpatialFilter is not None:
-        poLayer.SetSpatialFilter( poSpatialFilter )
+        if pszGeomField is not None:
+            iGeomField = poLayer.GetLayerDefn().GetGeomFieldIndex(pszGeomField)
+            if iGeomField >= 0:
+                poLayer.SetSpatialFilter( iGeomField, poSpatialFilter )
+            else:
+                print("WARNING: Cannot find geometry field %s." % pszGeomField)
+        else:
+            poLayer.SetSpatialFilter( poSpatialFilter )
 
 #/* -------------------------------------------------------------------- */
 #/*      Report various overall information.                             */
@@ -327,27 +340,53 @@ def ReportOnLayer( poLayer, pszWHERE, poSpatialFilter, options ):
     
     print( "Layer name: %s" % poDefn.GetName() )
 
-    if bVerbose_:
-        print( "Geometry: %s" % ogr.GeometryTypeToName( poDefn.GetGeomType() ) )
+    if bVerbose:
+        nGeomFieldCount = poLayer.GetLayerDefn().GetGeomFieldCount()
+        if nGeomFieldCount > 1:
+            for iGeom in range(nGeomFieldCount):
+                poGFldDefn = poLayer.GetLayerDefn().GetGeomFieldDefn(iGeom)
+                print( "Geometry (%s): %s" % (poGFldDefn.GetNameRef(), ogr.GeometryTypeToName( poGFldDefn.GetType() ) ))
+        else:
+            print( "Geometry: %s" % ogr.GeometryTypeToName( poDefn.GetGeomType() ) )
         
         print( "Feature Count: %d" % poLayer.GetFeatureCount() )
         
-        oExt = poLayer.GetExtent(True, can_return_null = True)
-        if oExt is not None:
-            print("Extent: (%f, %f) - (%f, %f)" % (oExt[0], oExt[1], oExt[2], oExt[3]))
-
-        if poLayer.GetSpatialRef() is None:
-            pszWKT = "(unknown)"
+        if nGeomFieldCount > 1:
+            for iGeom in range(nGeomFieldCount):
+                poGFldDefn = poLayer.GetLayerDefn().GetGeomFieldDefn(iGeom)
+                oExt = poLayer.GetExtent(True, geom_field = iGeom, can_return_null = True)
+                if oExt is not None:
+                    print("Extent (%s): (%f, %f) - (%f, %f)" % (poGFldDefn.GetNameRef(), oExt[0], oExt[2], oExt[1], oExt[3]))
         else:
-            pszWKT = poLayer.GetSpatialRef().ExportToPrettyWkt()
+            oExt = poLayer.GetExtent(True, can_return_null = True)
+            if oExt is not None:
+                print("Extent: (%f, %f) - (%f, %f)" % (oExt[0], oExt[2], oExt[1], oExt[3]))
 
-        print( "Layer SRS WKT:\n%s" % pszWKT )
+        if nGeomFieldCount > 1:
+            for iGeom in range(nGeomFieldCount):
+                poGFldDefn = poLayer.GetLayerDefn().GetGeomFieldDefn(iGeom)
+                if poGFldDefn.GetSpatialRef() is None:
+                    pszWKT = "(unknown)"
+                else:
+                    pszWKT = poGFldDefn.GetSpatialRef().ExportToPrettyWkt()
+                print( "SRS WKT (%s):\n%s" % (poGFldDefn.GetNameRef(), pszWKT) )
+        else:
+            if poLayer.GetSpatialRef() is None:
+                pszWKT = "(unknown)"
+            else:
+                pszWKT = poLayer.GetSpatialRef().ExportToPrettyWkt()
+            print( "Layer SRS WKT:\n%s" % pszWKT )
     
         if len(poLayer.GetFIDColumn()) > 0:
             print( "FID Column = %s" % poLayer.GetFIDColumn() )
     
-        if len(poLayer.GetGeometryColumn()) > 0:
-            print( "Geometry Column = %s" % poLayer.GetGeometryColumn() )
+        if nGeomFieldCount > 1:
+            for iGeom in range(nGeomFieldCount):
+                poGFldDefn = poLayer.GetLayerDefn().GetGeomFieldDefn(iGeom)
+                print( "Geometry Column %d = %s" % (iGeom + 1, poGFldDefn.GetNameRef() ))
+        else:
+            if len(poLayer.GetGeometryColumn()) > 0:
+                print( "Geometry Column = %s" % poLayer.GetGeometryColumn() )
 
         for iAttr in range(poDefn.GetFieldCount()):
             poField = poDefn.GetFieldDefn( iAttr )
@@ -363,18 +402,18 @@ def ReportOnLayer( poLayer, pszWHERE, poSpatialFilter, options ):
 #/* -------------------------------------------------------------------- */
     poFeature = None
 
-    if nFetchFID_ == ogr.NullFID and not bSummaryOnly_:
+    if nFetchFID == ogr.NullFID and not bSummaryOnly:
 
         poFeature = poLayer.GetNextFeature()
         while poFeature is not None:
             DumpReadableFeature(poFeature, options)
             poFeature = poLayer.GetNextFeature()
 
-    elif nFetchFID_ != ogr.NullFID:
+    elif nFetchFID != ogr.NullFID:
 
-        poFeature = poLayer.GetFeature( nFetchFID_ )
+        poFeature = poLayer.GetFeature( nFetchFID )
         if poFeature is None:
-            print( "Unable to locate feature id %d on this layer." % nFetchFID_ )
+            print( "Unable to locate feature id %d on this layer." % nFetchFID )
 
         else:
             DumpReadableFeature(poFeature, options)
@@ -409,10 +448,17 @@ def DumpReadableFeature( poFeature, options = None ):
         if 'DISPLAY_STYLE' not in options or EQUAL(options['DISPLAY_STYLE'], 'yes'):
             print("  Style = %s" % GetStyleString() )
 
-    poGeometry = poFeature.GetGeometryRef()
-    if poGeometry is not None:
+    nGeomFieldCount = poFeature.GetGeomFieldCount()
+    if nGeomFieldCount > 0:
         if 'DISPLAY_GEOMETRY' not in options or not EQUAL(options['DISPLAY_GEOMETRY'], 'no'):
-            DumpReadableGeometry( poGeometry, "  ", options)
+            for iField in range(nGeomFieldCount):
+                poGFldDefn = poFeature.GetDefnRef().GetGeomFieldDefn(iField)
+                poGeometry = poFeature.GetGeomFieldRef(iField)
+                if poGeometry is not None:
+                    sys.stdout.write("  ")
+                    if len(poGFldDefn.GetNameRef()) > 0 and nGeomFieldCount > 1:
+                        sys.stdout.write("%s = " % poGFldDefn.GetNameRef() )
+                    DumpReadableGeometry( poGeometry, "", options)
 
     print('')
 
